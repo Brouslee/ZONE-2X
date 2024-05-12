f4bf75981f3efea7016676efdec24c629262353c
Ssir11114 <sisirrobot@gmail.com>
2024-05-12 16:47:29 +0600

    Update Uptime.js

--- scripts/cmds/Uptime.js
+++ scripts/cmds/Uptime.js
@@ -1,28 +1,39 @@
 module.exports = {
-  config: {
-    name: "uptime",
-    aliases: ["up", "upt"],
-    version: "1.0",
-    author: "MR.AYAN",
-    role: 0,
-    shortDescription: {
-      en: "Displays the uptime of the bot."
-    },
-    longDescription: {
-      en: "Displays the amount of time that the bot has been running for."
-    },
-    category: "System",
-    guide: {
-      en: "Use {p}uptime to display the uptime of the bot."
-    }
-  },
-  onStart: async function ({ api, event, args }) {
-    const uptime = process.uptime();
-    const seconds = Math.floor(uptime % 60);
-    const minutes = Math.floor((uptime / 60) % 60);
-    const hours = Math.floor((uptime / (60 * 60)) % 24);
-    const days = Math.floor(uptime / (60 * 60 * 24));
-    const uptimeString = `\n${hours} 𝒉𝒐𝒖𝒓𝒔\n${minutes} 𝒎𝒊𝒏𝒖𝒕𝒆𝒔\n${seconds} 𝒔𝒆𝒄𝒐𝒏𝒅 `;
-    api.sendMessage(`𝑯𝒆𝒍𝒍𝒐 𝑴𝒂𝒔𝒕𝒆𝒓\n\n𝑻𝒉𝒆 𝑩𝒐𝒕 𝑯𝒂𝒔 𝑩𝒆𝒆𝒏 𝑹𝒖𝒏𝒏𝒊𝒏𝒈 𝑭𝒐𝒓 ${uptimeString}.`, event.threadID);
-  }
+	config: {
+		name: "uptime",
+		aliases: ["up"],
+      author: "yukinori",
+		role: 0,
+		shortDescription: {
+			en: "Show server uptime",
+		},
+		longDescription: {
+			en: "Shows the duration for which the server has been running",
+		},
+		category: "goatBot",
+		guide: {
+			en: "{p}uptime",
+		},
+	},
+
+	onStart: async function ({ api, message, threadsData }) {
+		const os = require("os");
+		const uptime = os.uptime();
+
+       const days = Math.floor(uptime / (60 * 60 * 24));
+       const hours = Math.floor((uptime / (60 * 60)) % 24);
+       const mins = Math.floor((uptime / 60) % 60);
+       const seconds = Math.floor(uptime % 60);
+
+		const system = `OS: ${os.platform()} ${os.release()}`;
+		const cores = `Cores: ${os.cpus().length}`;
+		const arch = `Architecture: ${os.arch()}`;
+		const totalMemory = `Total Memory: ${Math.round(os.totalmem() / (1024 * 1024 * 1024))} GB`;
+		const freeMemory = `Free Memory: ${Math.round(os.freemem() / (1024 * 1024 * 1024))} GB`;
+		const uptimeString = `Uptime: ${days} days, ${hours} hours, ${mins} minutes, and ${seconds} seconds`;
+
+		const response = `🕒 ${uptimeString}\n📡 ${system}\n🛡 ${cores}\n⚔ No AI Status\n📈 Total Users: ${threadsData.size}\n📉 Total Threads: ${threadsData.size}\n⚖ AI Usage: 0.0\n📊 RAM Usage: ${Math.round(process.memoryUsage().rss / (1024 * 1024))} MB\n💰 Total(RAM): ${Math.round(os.totalmem() / (1024 * 1024 * 1024))} GB\n💸 Current(RAM): ${Math.round(os.freemem() / (1024 * 1024 * 1024))} GB\n🛫 Ping: 15 ms\n🕰 Uptime(Seconds): ${Math.floor(process.uptime())}`;
+
+		message.reply(response);
+	},
 }; 
